7ac4ccd5faa88f578a326d4412663ffa9d5c17fb
equalsraf <undisclosed>
2020-06-14 20:06:42 +0100

    Update neovim-qt to v0.2.16

--- neovim-qt.rb
+++ neovim-qt.rb
@@ -1,8 +1,8 @@
 class NeovimQt < Formula
   desc "Neovim GUI, in Qt5"
   homepage "https://github.com/equalsraf/neovim-qt"
-  url "https://github.com/equalsraf/neovim-qt/archive/v0.2.13.tar.gz"
-  sha256 "382d6db482804e830d14b922caaef4500c325f3a380165d68b6e998d99deb837"
+  url "https://github.com/equalsraf/neovim-qt/archive/v0.2.16.tar.gz"
+  sha256 "fc1244e0b667688c684e540b6c09aa14a62cc441e4b4e7f742ed61cdf94937d6"
   head "https://github.com/equalsraf/neovim-qt.git"
 
   depends_on "cmake" => :build
