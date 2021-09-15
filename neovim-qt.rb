408fe218c3deccac575c4f1ed3704eba9a2b6595
Greg Werbin <gwerbin@understood.org>
2021-09-15 00:16:56 -0400

    Bump to v0.2.16.1 & support Linux

--- neovim-qt.rb
+++ neovim-qt.rb
@@ -1,8 +1,8 @@
 class NeovimQt < Formula
   desc "Neovim GUI, in Qt5"
   homepage "https://github.com/equalsraf/neovim-qt"
-  url "https://github.com/equalsraf/neovim-qt/archive/v0.2.16.tar.gz"
-  sha256 "fc1244e0b667688c684e540b6c09aa14a62cc441e4b4e7f742ed61cdf94937d6"
+  url "https://github.com/equalsraf/neovim-qt/archive/v0.2.16.1.tar.gz"
+  sha256 "971d4597b40df2756b313afe1996f07915643e8bf10efe416b64cc337e4faf2a"
   head "https://github.com/equalsraf/neovim-qt.git"
 
   depends_on "cmake" => :build
@@ -13,8 +13,15 @@ class NeovimQt < Formula
     mkdir "build" do
       system "cmake", "-DCMAKE_BUILD_TYPE=Release", ".."
       system "make"
-      prefix.install "bin/nvim-qt.app"
-      bin.install_symlink prefix/"nvim-qt.app/Contents/MacOS/nvim-qt"
+
+      on_macos do
+        prefix.install "bin/nvim-qt.app"
+        bin.install_symlink prefix/"nvim-qt.app/Contents/MacOS/nvim-qt"
+      end
+
+      on_linux do
+        bin.install "bin/nvim-qt"
+      end
     end
   end
 
